d3480b5d385f0c9ae7d74e219cc84a3b0c432bff
bvanhinsbergen <bvanhinsbergen@smatechnologies.com>
2026-02-16 10:12:43 +0100

    doc update - add release note

---
+++ docs/release-notes.md
@@ -0,0 +1,7 @@
+# Release Notes
+
+February 2026
+
+**26.0.0**
+
+Initial Release.
--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   mySidebar: [
     'index',
+	'release-notes',
 	'installation',
 	'agent-definition',
 	'task-definition',
